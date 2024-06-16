891cef67391944249768310525aabb353320615c
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-06-16 22:09:02 +0200

    ?

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,6 +5,7 @@ import { name } from './stores.js';
 import { env } from '$env/dynamic/public';
 const socket = io(env.PUBLIC_SOCKET_URL);
 
+let socketsListening = false;
 
 
 if (localStorage.getItem('name')) {
@@ -14,27 +15,33 @@ if (localStorage.getItem('name')) {
 
 
 
-
-socket.on('noName', () => {
-    goto('/');
-});
-socket.on('join', (pin) => {
-    goto('/' + pin);
-});
-
-socket.on('name', (newName) => {
-    name.set(newName);
-});
+function listenToSockets() {
+    socketsListening = true;
 
 
-socket.on('gameEnded', () => {
-    name.set('');
-    goto('/');
-});
+    socket.on('noName', () => {
+        goto('/');
+    });
+    socket.on('join', (pin) => {
+        goto('/' + pin);
+    });
 
-socket.on("joinError", (err) => {
-    alert(err);
-    goto('/');
-});
+    socket.on('name', (newName) => {
+        name.set(newName);
+    });
+
+
+    socket.on('gameEnded', () => {
+        name.set('');
+        goto('/');
+    });
+
+    socket.on("joinError", (err) => {
+        alert(err);
+        goto('/');
+    });
+}
+
+if (!socketsListening) listenToSockets();
 
 export default socket;
